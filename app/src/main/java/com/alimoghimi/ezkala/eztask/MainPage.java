bb5e50887b333b957d0409a8e2323947448c2b68
Ali Moghimi <ali_moghimii@yahoo.com>
2019-01-17 15:12:51 +0330

    second

--- app/src/main/java/com/alimoghimi/ezkala/eztask/MainPage.java
+++ app/src/main/java/com/alimoghimi/ezkala/eztask/MainPage.java
@@ -1,13 +1,51 @@
 package com.alimoghimi.ezkala.eztask;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainPage extends AppCompatActivity {
+
+    int progressCounter = 0;
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(progressCounter < 100)
+                {
+                    progressCounter++;
+                    android.os.SystemClock.sleep(50);
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progressCounter);
+
+                            if(progressCounter == 100)
+                            {
+                                Intent SignIn = new Intent(getApplicationContext(),SignIn.class);
+                                startActivity(SignIn);
+                            }
+
+                        }
+                    });
+
+                }
+
+            }
+        }).start();
+
     }
 }
